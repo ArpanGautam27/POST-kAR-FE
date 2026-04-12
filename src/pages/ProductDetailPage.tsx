f175e8a93b79b7b4b5c2f65c76b2cc4e154d76da
Arpan Gautam <arpangautam2702@gmail.com>
2026-04-12 21:08:53 +0530

    Revert "added google play link and whatsapp number"
    This reverts commit e5cbbb90104d4c0844a449263127668955f188d2.

--- src/pages/ProductDetailPage.tsx
+++ src/pages/ProductDetailPage.tsx
@@ -14,7 +14,7 @@ import './ProductDetailPage.css';
 const productService = ProductService.getInstance();
 
 const WHATSAPP_NUMBER = '917579122216';
-const PLAY_STORE_URL = 'https://play.google.com/store/apps/details?id=com.postkar.arapp';
+const PLAY_STORE_URL = 'https://play.google.com/store/apps/details?id=com.postkar';
 
 function getWhatsAppProductUrl(productName: string): string {
   const msg = encodeURIComponent(`Hi, I want to buy this Post-kAR product: ${productName}`);
